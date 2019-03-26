85d0096789964b3980c4c03a298cf6d608eaa5bb
Dvontre Coleman <dvontrec@gmail.com>
2019-03-26 16:26:11 -0400

    Add user create mutation

--- python-graphene/schema.py
+++ python-graphene/schema.py
@@ -1,19 +1,34 @@
 import graphene
 import json
+import uuid
 from datetime import datetime
 
+
 # Creates a user data class
-
-
 class User (graphene.ObjectType):
-    id = graphene.ID()
+    id = graphene.ID(default_value=uuid.uuid4())
     username = graphene.String()
-    created_at = graphene.DateTime()
+    created_at = graphene.DateTime(default_value=datetime.now())
+
+
+# Creates a class for creating users
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(User)
+    # defined the arguments the mutation recieves
+
+    class Arguments:
+        # Username will be the only field we pass to the mutation
+        username = graphene.String()
+
+    # resolver functions are always called mutate
+    def mutate(self, info, username):
+        user = User(username=username)
+        return CreateUser(user=user)
 
 
 # creates a root query type that subclasses from graphine.ObjectType
 class Query (graphene.ObjectType):
-    # Users query will return a list of users
+        # Users query will return a list of users
     users = graphene.List(User, limit=graphene.Int())
     # specifies that the hello query will return a string
     hello = graphene.String()
@@ -30,25 +45,48 @@ class Query (graphene.ObjectType):
 
     # resolve function for getting users
     def resolve_users(self, info, limit=None):
+        # returns a list of users
         return [
-            User(id="1", username="Q", created_at=datetime.now()),
-            User(id="2", username="Smoke", created_at=datetime.now())
+            User(username="Q", created_at=datetime.now()),
+            User(username="Smoke", created_at=datetime.now())
         ][:limit]  # Slices the list to only include the limit of users returned
 
 
-schema = graphene.Schema(query=Query)
+# adds a Mutation class
+class Mutation(graphene.ObjectType):
+    # createUser query used to make new users
+    create_user = CreateUser.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 # queries from the user query with data needed specified
+# result = schema.execute(
+#     '''
+#   {
+#     users{
+#       id,
+#       username
+#       createdAt
+#     }
+#   }
+#   '''
+# )
+
+# queries a mutation to create a user
 result = schema.execute(
     '''
-  {
-    users{
-      id,
-      username
-      createdAt
+  mutation($username: String){
+    createUser(username: $username){
+      user{
+        id
+        username
+        createdAt
+      }
     }
   }
-  '''
+  ''',
+    variable_values={'username': 'Dev'}
 )
 
 #  prints the query results as json
